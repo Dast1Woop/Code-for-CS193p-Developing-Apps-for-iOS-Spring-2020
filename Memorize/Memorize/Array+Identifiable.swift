ec55d07089c45043cd1411ccdea30ede7d533047
myl <myldast1@126.com>
2020-12-17 17:57:06 +0800

    new comment

--- Memorize/Memorize/Array+Identifiable.swift
+++ Memorize/Memorize/Array+Identifiable.swift
@@ -12,6 +12,8 @@ import Foundation
 //extension Array<Element> where Element: Identifiable{
 
 extension Array where Element: Identifiable{
+    
+    //Element:swift 自定义的占位符，不是所有的 Element 都能被 T 代替，比如此处
     func firstIndex(matching element:Element) -> Int? {
         for i in 0..<self.count {
             let lEle = self[i]
